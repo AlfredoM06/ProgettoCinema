f69d281f8323a41253a98675c596d111ef19b52f
EricaLan1 <ericalandolfo1@gmail.com>
2026-05-11 20:52:02 +0200

    aggiunta sezione menu e gadget con fix css

--- PJ Cinema/js/home.js
+++ PJ Cinema/js/home.js
@@ -10,3 +10,19 @@ cards.forEach(card => {
     overlay.style.opacity = '0';
   });
 });
+
+const titoli = document.querySelectorAll(".titoloLinea");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+    }
+  });
+}, {
+  threshold: 0.5
+});
+
+titoli.forEach(titolo => {
+  observer.observe(titolo);
+});
