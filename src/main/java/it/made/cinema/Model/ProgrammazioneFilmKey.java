f2b4ff5718cb4204d26753c9158f73610524f4c6
Dom <politellidomenico00@gmail.com>
2026-05-19 17:41:22 +0200

    Creata classe key

---
+++ src/main/java/it/made/cinema/Model/ProgrammazioneFilmKey.java
@@ -0,0 +1,20 @@
+package it.made.cinema.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+@ToString @EqualsAndHashCode
+@Table(name=)
+public class ProgrammazioneFilmKey {
+
+}
