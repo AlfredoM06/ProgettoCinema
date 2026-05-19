61d73fbbea5240b7d27f826dd0ebda3d55f1f1b4
Dom <politellidomenico00@gmail.com>
2026-05-19 18:20:16 +0200

    Model PostiOccupati

---
+++ src/main/java/it/made/cinema/Model/PostiOccupati.java
@@ -0,0 +1,14 @@
+package it.made.cinema.Model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+@ToString @EqualsAndHashCode
+@Table(name=)
+public class PostiOccupati {
+
+}
