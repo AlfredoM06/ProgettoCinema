628f7a907c3febe2fed1eae7447a125eb2c26c0d
Dom <politellidomenico00@gmail.com>
2026-05-22 18:31:12 +0200

    Metodo dettaglio prossimamente aggiunto

--- src/main/java/it/made/cinema/Controller/ProssimamenteController.java
+++ src/main/java/it/made/cinema/Controller/ProssimamenteController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import it.made.cinema.Model.Film;
@@ -25,5 +26,11 @@ public class ProssimamenteController {
 		//bisogna inserire un filtro con data odierna e top5 per numero prenotazioni
 		
 		return "suca";
+	}
+	
+	@GetMapping("/dettagli/{id}")
+	private String dettagliProssimamente(@PathVariable("id") Integer id, Model model) {
+		model.addAttribute("film", RepoFilm.findById(id).get());
+		return "filmDettaglio";
 	}
 }
