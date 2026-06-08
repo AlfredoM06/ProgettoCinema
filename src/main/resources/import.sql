09992d259d97f36f372348cf9112c5f84789c1df
AlfredoM06 <Alfredomanfredi06@gmail.com>
2026-06-08 18:17:58 +0200

    inzio aggiunta import

---
+++ src/main/resources/import.sql
@@ -0,0 +1,95 @@
+--film
+INSERT INTO db_cinema.films
+(archiviato, data_di_uscita, durata, prezzo, scadenza, distribuzione, formato, regista, titolo, img_cover, img_logo, img_poster, lingue, `cast`, descrizione)
+VALUES(0, '', 0, 0, '', '', '', '', '', '', '', '', '', '', '');
+INSERT INTO db_cinema.films
+(archiviato, data_di_uscita, durata, prezzo, scadenza, distribuzione, formato, regista, titolo, img_cover, img_logo, img_poster, lingue, `cast`, descrizione)
+VALUES(0, '', 0, 0, '', '', '', '', '', '', '', '', '', '', '');
+INSERT INTO db_cinema.films
+(archiviato, data_di_uscita, durata, prezzo, scadenza, distribuzione, formato, regista, titolo, img_cover, img_logo, img_poster, lingue, `cast`, descrizione)
+VALUES(0, '', 0, 0, '', '', '', '', '', '', '', '', '', '', '');
+INSERT INTO db_cinema.films
+(archiviato, data_di_uscita, durata, prezzo, scadenza, distribuzione, formato, regista, titolo, img_cover, img_logo, img_poster, lingue, `cast`, descrizione)
+VALUES(0, '', 0, 0, '', '', '', '', '', '', '', '', '', '', '');
+INSERT INTO db_cinema.films
+(archiviato, data_di_uscita, durata, prezzo, scadenza, distribuzione, formato, regista, titolo, img_cover, img_logo, img_poster, lingue, `cast`, descrizione)
+VALUES(0, '', 0, 0, '', '', '', '', '', '', '', '', '', '', '');
+INSERT INTO db_cinema.films
+(archiviato, data_di_uscita, durata, prezzo, scadenza, distribuzione, formato, regista, titolo, img_cover, img_logo, img_poster, lingue, `cast`, descrizione)
+VALUES(0, '', 0, 0, '', '', '', '', '', '', '', '', '', '', '');
+INSERT INTO db_cinema.films
+(archiviato, data_di_uscita, durata, prezzo, scadenza, distribuzione, formato, regista, titolo, img_cover, img_logo, img_poster, lingue, `cast`, descrizione)
+VALUES(0, '', 0, 0, '', '', '', '', '', '', '', '', '', '', '');
+INSERT INTO db_cinema.films
+(archiviato, data_di_uscita, durata, prezzo, scadenza, distribuzione, formato, regista, titolo, img_cover, img_logo, img_poster, lingue, `cast`, descrizione)
+VALUES(0, '', 0, 0, '', '', '', '', '', '', '', '', '', '', '');
+INSERT INTO db_cinema.films
+(archiviato, data_di_uscita, durata, prezzo, scadenza, distribuzione, formato, regista, titolo, img_cover, img_logo, img_poster, lingue, `cast`, descrizione)
+VALUES(0, '', 0, 0, '', '', '', '', '', '', '', '', '', '', '');
+
+--generi
+INSERT INTO db_cinema.genere_films
+(nome)
+VALUES('');
+INSERT INTO db_cinema.genere_films
+(nome)
+VALUES('');
+INSERT INTO db_cinema.genere_films
+(nome)
+VALUES('');
+INSERT INTO db_cinema.genere_films
+(nome)
+VALUES('');
+INSERT INTO db_cinema.genere_films
+(nome)
+VALUES('');
+INSERT INTO db_cinema.genere_films
+(nome)
+VALUES('');
+INSERT INTO db_cinema.genere_films
+(nome)
+VALUES('');
+INSERT INTO db_cinema.genere_films
+(nome)
+VALUES('');
+
+--gadgets
+--INSERT INTO db_cinema.gadgets
+--(prezzo, quantita, nome, img, img_banner, descrizione)
+--VALUES(0, 0, '', '', '', '');
+
+--posti
+INSERT INTO db_cinema.posti
+(prezzo_posto, tipo)
+VALUES(0, '');
+INSERT INTO db_cinema.posti
+(tipo)
+VALUES('');
+INSERT INTO db_cinema.posti
+(tipo)
+VALUES('');
+
+--sale
+INSERT INTO db_cinema.sale
+(numero_posti, tipo)
+VALUES( 0, '');
+INSERT INTO db_cinema.sale
+(numero_posti, tipo)
+VALUES( 0, '');
+INSERT INTO db_cinema.sale
+(numero_posti, tipo)
+VALUES( 0, '');
+INSERT INTO db_cinema.sale
+(numero_posti, tipo)
+VALUES( 0, '');
+
+--ruoli
+--INSERT INTO db_cinema.ruoli
+--(nome)
+--VALUES('');
+
+--utenti
+--INSERT INTO db_cinema.utenti
+--(membership, password, ruolo, username, email)
+--VALUES(0, '', '', '', '');
+
