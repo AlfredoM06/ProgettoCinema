41c69de1156dc015be0d18483022a4d5e624053f
AlfredoM06 <Alfredomanfredi06@gmail.com>
2026-06-23 18:26:08 +0200

    creata nuovo model e relativo controller

--- src/main/java/it/made/cinema/Controller/ProssimamenteController.java
+++ src/main/java/it/made/cinema/Controller/ProssimamenteController.java
@@ -19,25 +19,25 @@ public class ProssimamenteController {
 
     //c'è la pagina
     //lista film, dettagli film, log in per la prenotazione,
-	@Autowired
-	private IRepoFilm repoFilm;
-	@GetMapping
-	public String listaProssimamente(Model model) {
-		//bisogna inserire un filtro con data odierna e top5 per numero prenotazioni
-		//List<Film> lsitafilm = repoFilm.findAll();
-		//collegare il filtro a thymeleaf cosi da far generare le card con già i film filtrati
-		List<Film> risultatoRicerca = repoFilm.findByAllDate();
-		model.addAttribute("filmProssimamente", risultatoRicerca);
-		//model.addAttribute(lsitafilm);
-		return "prossimamente";
-	}
-	
-	@GetMapping("/dettagli/{id}")
-	public String dettagliProssimamente(@PathVariable("id") Integer id, Model model) {
-		model.addAttribute("film", repoFilm.findById(id).get());
-		return "filmDettaglio";
-	}
+    @Autowired
+    private IRepoFilm repoFilm;
 
+    @GetMapping
+    public String listaProssimamente(Model model) {
+        //bisogna inserire un filtro con data odierna e top5 per numero prenotazioni
+        //List<Film> lsitafilm = repoFilm.findAll();
+        //collegare il filtro a thymeleaf cosi da far generare le card con già i film filtrati
+        List<Film> risultatoRicerca = repoFilm.findByAllDate();
+        model.addAttribute("filmProssimamente", risultatoRicerca);
+        //model.addAttribute(lsitafilm);
+        return "prossimamente";
+    }
+
+    @GetMapping("/dettagli/{id}")
+    public String dettagliProssimamente(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("film", repoFilm.findById(id).get());
+        return "filmDettaglio";
+    }
 
 
 }
---
+++ src/main/java/it/made/cinema/Model/Partnership.java
@@ -0,0 +1,10 @@
+package it.made.cinema.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "partnerships")
+public class Partnership {
+
+}
