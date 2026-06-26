fddf56698966a06f06120382482c57c755d9ade0
AlfredoM06 <Alfredomanfredi06@gmail.com>
2026-06-26 19:11:18 +0200

    aggiunto set up per dto nuovo

---
+++ src/main/java/it/made/cinema/Model/DTO/ListaProgFiltroDTO.java
@@ -0,0 +1,14 @@
+package it.made.cinema.Model.DTO;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class ListaProgFiltroDTO implements Serializable {
+}
