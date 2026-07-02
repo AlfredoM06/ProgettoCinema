08b0c8bbbb68e9c745f2a35f3989e84f1aa9d361
Dom <politellidomenico00@gmail.com>
2026-07-02 19:49:46 +0200

    fixati due errori di battitura

--- src/main/java/Scheduler/EmailCompleannoJob.java
+++ src/main/java/Scheduler/EmailCompleannoJob.java
@@ -1,6 +1,7 @@
 package Scheduler;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,20 +27,22 @@ public class EmailCompleannoJob {
 	public void emailCompleanno() {
 		LocalDate oggi = LocalDate.now();
 		List<Utente> utenti = repoUtenti.findAll();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		String scadenza = oggi.plusDays(6).format(formatter);
 		
 		for (Utente utente : utenti) {
 			Boolean compleanno = utente.getDataNascita().getMonthValue() == oggi.getMonthValue()
 					&& utente.getDataNascita().getDayOfMonth() == oggi.getDayOfMonth();
 			
 			Boolean giaMandataQuestAnno = utente.getAnnoUltimaMailCompleanno() != null
-					&& utente.getAnnoUltimaMailCompleanno() != oggi.getYear();
+					&& utente.getAnnoUltimaMailCompleanno() == oggi.getYear();
 			
 		if(compleanno && !giaMandataQuestAnno) {
 			SimpleMailMessage messaggio = new SimpleMailMessage();
 			messaggio.setTo(utente.getEmail());
 			messaggio.setSubject("Buon compleanno, " + utente.getNome()+" "+utente.getCognome()+"!");
-			messaggio.setText("Tanti auguri da tutto lo staff di Siediti & Guarda! Per questo evento straordinario abbiamo deciso di reglarti un codice omaggio da poter utilizzare fino al "+oggi.plusDays(6)
-			+"per gustarti un film gratuitamente! Il codice omaggio è: Capocchiabagnata ");
+			messaggio.setText("Tanti auguri da tutto lo staff di Siediti & Guarda! Per questo evento straordinario abbiamo deciso di regalarti un codice omaggio da poter utilizzare fino al "+ scadenza 
+			+" per gustarti un film gratuitamente! Il codice omaggio è: Capocchiabagnata ");
 			mailSender.send(messaggio);
 			
 			utente.setAnnoUltimaMailCompleanno(oggi.getYear());
