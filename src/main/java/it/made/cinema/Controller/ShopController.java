3b2971ebae41746c1192e310ca1e3b13b01e497f
Dom <politellidomenico00@gmail.com>
2026-07-07 17:38:47 +0200

    onlyfans

--- src/main/java/it/made/cinema/Controller/ShopController.java
+++ src/main/java/it/made/cinema/Controller/ShopController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/shop")
+@RequestMapping("/cinefans")
 public class ShopController {
 
     //ancora da fare
