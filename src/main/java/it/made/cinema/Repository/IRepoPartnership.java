873788a8d8df20c264ce52b7065096dd63de23fa
Dom <politellidomenico00@gmail.com>
2026-08-11 18:44:36 +0200

    finito controller film e partnership

--- src/main/java/it/made/cinema/Repository/IRepoPartnership.java
+++ src/main/java/it/made/cinema/Repository/IRepoPartnership.java
@@ -4,4 +4,6 @@ import it.made.cinema.Model.Partnership;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IRepoPartnership extends JpaRepository<Partnership, Integer> {
+
+	Partnership findByFilmId(Integer id);
 }
