85ae549fb7c8f0cb5cc2ab593cd2bdeff7275d26
Dom <politellidomenico00@gmail.com>
2026-09-02 22:48:17 +0200

    Altro bug fix

--- src/main/java/it/made/cinema/Repository/IRepoCarrello.java
+++ src/main/java/it/made/cinema/Repository/IRepoCarrello.java
@@ -4,10 +4,14 @@ package it.made.cinema.Repository;
 import it.made.cinema.Model.Carrello;
 import it.made.cinema.Model.Utente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface IRepoCarrello extends JpaRepository<Carrello, Integer> {
 
     public Carrello findByUtente(Utente utente);
+    @Query("SELECT c FROM Carrello c LEFT JOIN FETCH c.listaOfferte WHERE c.utente = :utente")
+    Carrello findByUtenteWithOfferte(@Param("utente") Utente utente);
 }
