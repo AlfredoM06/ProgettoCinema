32d5c24bd6117b1dde7b43609ad51bb229a9f8a5
AlfredoM06 <Alfredomanfredi06@gmail.com>
2026-09-08 19:23:22 +0200

    bug fix 10(aggiusta una cosa e se ne rompe un'altra)

--- src/main/java/it/made/cinema/Controller/OfferteController.java
+++ src/main/java/it/made/cinema/Controller/OfferteController.java
@@ -44,7 +44,7 @@ public class OfferteController {
         }
         List<ListaOffertaDTO> offerteDTO = new ArrayList<ListaOffertaDTO>();
         for (Offerta offerta : offerte) {
-            offerteDTO.add(new ListaOffertaDTO(offerta.getId(), offerta.getNome(), offerta.getGenere(), offerta.getDescrizione(), offerta.getImgBanner(), offerta.getPrezzo(), null, offerta.getDataInizio()));
+            offerteDTO.add(new ListaOffertaDTO(offerta.getId(), offerta.getNome(), offerta.getGenere(), offerta.getDescrizione(), offerta.getImgBanner(), offerta.getPrezzo(), null, offerta.getDataInizio(), null));
         }
         return offerteDTO;
     }
