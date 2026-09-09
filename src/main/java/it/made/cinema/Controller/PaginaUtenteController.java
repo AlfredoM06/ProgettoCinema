c57857d7cd07aecbb4fdca0be44cd0580266097b
AlfredoM06 <Alfredomanfredi06@gmail.com>
2026-09-09 19:22:39 +0200

    tolti commenti

--- src/main/java/it/made/cinema/Controller/PaginaUtenteController.java
+++ src/main/java/it/made/cinema/Controller/PaginaUtenteController.java
@@ -47,9 +47,7 @@ public class PaginaUtenteController {
         return "utente-profilo";
     }
 
-    //                  FATTI:
-    //1) I biglietti prenotati e acquistati con i relativi dati i quali verranno cancellati dopo 1 settimana per eventuali rimborsi.
-    // va aggiunto attributo in posti occupati che lega utente con posto occupato (tramite id utente) tramite l'id posto occupato andiamo in programmazione e vediamo quale film ha prenotato/acquistato.
+
     @GetMapping("/acquisti")
     public @ResponseBody List<PostiOccupatiDTO> bigliettiAcquistati(Authentication authentication) {
         DatabaseUserDetails userDetails = (DatabaseUserDetails) authentication.getPrincipal();
@@ -140,21 +138,4 @@ public class PaginaUtenteController {
         return nPunti;
     }
 
-    //10) Solo per l'anteprima dei film i posti vip saranno riservati ai possessori di carta myUci,
-    // se i posti non verranno comprati entro 4 ore prima dell'anteprima verranno sbloccati i posti al pubblico.
-    // (Ragionamento da far verificare ad emilio = fare un if per capire se hai la carta o meno, poi far si che solo chi ha la carta può accedere ai posti vip, per gli altri fare un data di inizio minus 4 ore data di inzio)
-    // (soluzione emilio la parte di impedire agli altri di prenotare se non hanno la carte da far fare a erica tramite un if che mostra solo a chi è tesserato la possibilità di prendersi quei posti, erica si richiama un metodo che gli facciamo noi
-    // in cui calcoliamo se mancano effettivamente 4 ore all'inizio dell'anteprima e vedere se è tesserato o meno)
-
-    //6) Biglietto omaggio per il tuo compleanno da usare entro i 6 giorni successivi alla data di comp.(FATTO DA DOM)
-    //7) Prezzo base bliglietto 7 euro, prezzo con card myUCI è 5 euro.(controller membership) (FATTO) metodo non più necessario poichè è stato accorpato al 9 e spostato sul controller del biglietto
-    //8) C'è lo sconto al bar del 10% sui gadget relativi ad un film (se hai la card punti,controller membership).(FATTO)
-    //9) Film e o menu gratis/scontati a seconda dei punti.(controller membership) spostano in biglietto controller. (FATTO)
-    //11) Per determinate cose si hanno dei punti extra (es. chi vede i film sponsorizzati riceveranno punti extra) e per ogni anno di possesso della card si guadagno punti.(FINITO)
-    //12) fare metodo per i punti della carta che ad ogni euro speso equivale a 10 punti da collegare all'11.(FINITO)
-    // Qunado fai la card myS&G ti danno punti di benvenuto, all'acquisto della card/abbonamento si riceveranno punti da aggiungere alla myS&G;
-    // la carta punti e le carte ricaricabili sono due entità separate.
-    // Le carte ricariabili saranno ad uso singolo: nel senso che se scade o finisci gli usi limitati, le opzioni sono due:
-    // op 1 si rinnova la carta, op 2 si cambia tipo di carta.
-    // Se ad esempio passo da basic a plus sarà attiva solo la plus tipo piano di netflix.
 }
