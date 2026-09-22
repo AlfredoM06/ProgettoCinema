e438050019f174ebba132bc204b421494715829a
AlfredoM06 <Alfredomanfredi06@gmail.com>
2026-09-22 18:47:36 +0200

    bug fix

--- src/main/java/it/made/cinema/Security/SecurityConfiguration.java
+++ src/main/java/it/made/cinema/Security/SecurityConfiguration.java
@@ -62,6 +62,7 @@ public class SecurityConfiguration {
                         .requestMatchers("/", "/login", "/logout").permitAll()
                         .requestMatchers("/offete", "/cinefans").permitAll()
                         .requestMatchers("/inSala/**", "/prossimamente/**", "/partnership/**").permitAll()
+                        .requestMatchers("/css/**", "/js/**", "/img/**", "/webjars/**").permitAll()
 
                         // --- membership: pagina pubblica, acquisto protetto ---
                         .requestMatchers(HttpMethod.POST, "/membership/membershipAcquistata").hasAnyAuthority("Admin", "User")
